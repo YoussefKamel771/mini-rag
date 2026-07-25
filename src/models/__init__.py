b71a6d19196b51b6ca32695d5af7055b131a1d6d
Youssef <yousefkamel282@gmail.com>
2026-07-25 14:08:41 +0300

    added chunk model for managing data base chunk collection

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,5 @@
 from .enums.ResponseEnums import ResponseSignal
 from .enums.ProcessingEnum import ProcessingEnum
+from .ProjectModel import ProjectModel
+from .ChunkModel import ChunkModel
+from .db_schemas.data_chunk import DataChunk
--- src/models/db_schemas/project.py
+++ src/models/db_schemas/project.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 from bson.objectid import ObjectId
 
@@ -6,7 +6,7 @@ class Project(BaseModel):
     id: Optional[ObjectId] = Field(None, alias="_id")
     project_id: str = Field(..., min_length=1)
 
-    @validator('project_id')
+    @field_validator('project_id')
     def validate_project_id(cls, value):
         if not value.isalnum():
             raise ValueError('project_id must be alphanumeric')
