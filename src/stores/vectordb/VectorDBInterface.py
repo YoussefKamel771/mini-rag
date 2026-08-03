c12da349d4d12bedde643f8d9cc711b45875ed9a
Youssef <yousefkamel282@gmail.com>
2026-08-03 21:29:20 +0300

    added the search schema to the interface

--- src/stores/vectordb/VectorDBInterface.py
+++ src/stores/vectordb/VectorDBInterface.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Dict
+from models import RetrievedDocument
 
 class VectorDBInterface(ABC):
     @abstractmethod
@@ -45,5 +46,5 @@ class VectorDBInterface(ABC):
         pass
 
     @abstractmethod
-    def search_by_vector(self, collection_name: str, vector: list, limit: int):
+    def search_by_vector(self, collection_name: str, vector: list, limit: int) -> List[RetrievedDocument]:
         pass
