3255e36658ce59a340255deabbacd168838de716
Youssef <yousefkamel282@gmail.com>
2026-08-03 21:34:27 +0300

    added an endpoint for answering rag questions

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,7 +4,7 @@ from .enums.AssetTypeEnum import AssetTypeEnum
 from .ProjectModel import ProjectModel
 from .ChunkModel import ChunkModel
 from .AssetModel import AssetModel
-from .db_schemas.data_chunk import DataChunk
+from .db_schemas.data_chunk import DataChunk, RetrievedDocument
 from .db_schemas.project import Project
 from .db_schemas.asset import Asset
 
